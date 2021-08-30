acb403d5e76c3c37cf23fda015ef41d878fee0f4
Burak Yılmam <61635780+Burakyilmam@users.noreply.github.com>
2021-08-31 00:38:35 +0300

    Add files via upload

---
+++ C# Listeleme ve Checkbox Seçili Olanları Güncelleme/Demo/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBtn = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBtn
+            // 
+            this.listBtn.Location = new System.Drawing.Point(344, 397);
+            this.listBtn.Name = "listBtn";
+            this.listBtn.Size = new System.Drawing.Size(75, 23);
+            this.listBtn.TabIndex = 0;
+            this.listBtn.Text = "Listele";
+            this.listBtn.UseVisualStyleBackColor = true;
+            this.listBtn.Click += new System.EventHandler(this.listBtn_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(597, 182);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Surname";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Birth Date";
+            this.columnHeader4.Width = 100;
+            // 
+            // listView1
+            // 
+            this.listView1.AllowColumnReorder = true;
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 34);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1121, 698);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Location = new System.Drawing.Point(0, 2);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(112, 30);
+            this.updateBtn.TabIndex = 2;
+            this.updateBtn.Text = "Güncelle";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1121, 732);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.listBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button listBtn;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button updateBtn;
+    }
+}
+
---
+++ C# Listeleme ve Checkbox Seçili Olanları Güncelleme/Demo/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        string constring = ("Data source = localhost; initial catalog = WindowsForm; integrated security = True;");
+        SqlConnection cn = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            constring = ConfigurationManager.AppSettings["connectionstrings"];
+            cn = new SqlConnection(constring);
+            listele();
+        }
+        public void listele()
+        {
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("select * from Kullanici", cn);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                ListViewItem ekle = new ListViewItem();
+                ekle.Text = dr["ID"].ToString();
+                ekle.SubItems.Add(dr["Name"].ToString());
+                ekle.SubItems.Add(dr["Surname"].ToString());
+                ekle.SubItems.Add(dr["BirthDate"].ToString());
+                listView1.Items.Add(ekle);
+            }
+            cn.Close();
+        }
+        private void listBtn_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            cn.Open();
+            try
+            {
+                int i = 0;
+                foreach (ListViewItem item in listView1.Items)
+                {
+
+                    if (item.Checked == true)
+                    {
+                        i++;
+                        SqlCommand cmd = new SqlCommand("Update Kullanici set Name='Ali' where ID=" + item.Text, cn);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                if (i == 0)
+                {
+                    MessageBox.Show("Kayıt Seçiniz");
+                    listView1.Items.Clear();
+                    cn.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Kayıt Güncellendi.");
+                    listView1.Items.Clear();
+                    cn.Close();
+                }
+                listele();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
